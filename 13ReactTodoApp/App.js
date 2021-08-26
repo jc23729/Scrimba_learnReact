4ed5f9e577c0df6820aa665990cdea4bfbf07560
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-08-26 10:48:58 -0400

    Update App.js

--- 13ReactTodoApp/App.js
+++ 13ReactTodoApp/App.js
@@ -2,9 +2,19 @@ import React from 'react'
 
 export default function App() {
     return (
-        <div>
-            <input type="checkbox" />
-            <p>Placeholder text here</p> 
-        </div>
-    )
+      <div>
+        <input type="checkbox" />
+        <p>Placeholder text here</p>
+
+        <input type="checkbox" />
+        <p>Placeholder text here</p>
+
+        <input type="checkbox" />
+        <p>Placeholder text here</p>
+
+        <input type="checkbox" />
+         <p>Placeholder text here</p>
+        
+      </div>
+    );
 }
