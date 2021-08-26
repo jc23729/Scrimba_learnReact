187a2884f82aabe553888b2482393a91ff8ab208
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-08-26 10:48:10 -0400

    update

---
+++ 13ReactTodoApp/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+    return (
+        <div>
+            <input type="checkbox" />
+            <p>Placeholder text here</p> 
+        </div>
+    )
+}
--- 13ReactTodoApp/index.js
+++ 13ReactTodoApp/index.js
@@ -3,3 +3,11 @@
 // Create the <App /> component from scratch
 // Have the <App /> component render 3 or 4 checkboxes with paragraphs or spans next to it
 // like you're making a todo list with some hard-coded items on it
+
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
