beea77ad7b1f66c9e9cad27f07033f303c6f48db
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-08-26 10:30:44 -0400

    update

---
+++ components/MainContent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function MainContent() {
+    return (
+        <div>
+            <p>MainContent</p>
+            
+        </div>
+    )
+}
+export default MainContent
